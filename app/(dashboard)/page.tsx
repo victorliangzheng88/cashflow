b602163740ced0280852902358f708643c5a27f4
Victor Liang Zheng <60474948+victorliangzheng88@users.noreply.github.com>
2024-05-17 21:37:14 -0400

    feat: accounts post api

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,14 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
 
 export default function Home() {
-  return (
-    <p>HomePage</p>
-  )
+  const { onOpen } = useNewAccount();
 
+  return (
+    <div>
+      <Button onClick={onOpen}>Add an account</Button>
+    </div>
+  );
 }
---
+++ features/accounts/components/new-account-sheet.tsx
@@ -0,0 +1,49 @@
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+} from "@/components/ui/sheet";
+import { useNewAccount } from "../hooks/use-new-account";
+import { AccountForm } from "./account-form";
+import { insertAccountSchema } from "@/db/schema";
+import { z } from "zod";
+import { useCreateAccount } from "../api/use-create-account";
+
+const formSchema = insertAccountSchema.pick({
+  name: true,
+});
+
+type FormValues = z.input<typeof formSchema>;
+
+export const NewAccountSheet = () => {
+  const { isOpen, onClose } = useNewAccount();
+
+  const mutation = useCreateAccount();
+
+  const onSubmit = (values: FormValues) => {
+    mutation.mutate(values, {
+      onSuccess: () => {
+        onClose();
+      },
+    });
+  };
+
+  return (
+    <Sheet open={isOpen} onOpenChange={onClose}>
+      <SheetContent className="space-y-4">
+        <SheetHeader>New Account</SheetHeader>
+        <AccountForm
+          onSubmit={onSubmit}
+          disabled={mutation.isPending}
+          defaultValues={{
+            name: "",
+          }}
+        />
+        <SheetDescription>
+          Create a new account to track your transactions
+        </SheetDescription>
+      </SheetContent>
+    </Sheet>
+  );
+};
